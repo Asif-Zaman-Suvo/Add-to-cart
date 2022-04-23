d47a165b2c95ec88dcd7223063b0a564c999f54a
asifshuvo119181@gmail.com <asifshuvo119181@gmail.com>
2022-04-23 16:17:57 +0600

    local storage added

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,13 @@ export const UserContext = createContext();
 
 function MyApp({ Component, pageProps }) {
   const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    const items = JSON.parse(localStorage.getItem("cart"));
+    if (items) {
+      setCart(items);
+    }
+  },[]);
   return (
     <>
       <UserContext.Provider value={[cart, setCart]}>
